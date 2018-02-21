0ff436cd6e53ab2acb3dc2ecbf2a38573444c3b5
Tomasz Pleśniak <31666201+Tomke997@users.noreply.github.com>
2018-02-21 15:44:53 +0100

    vacuum cleaners suck

--- src/main/Main.java
+++ src/main/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        System.out.println("Marin suck dick");
     }
 
     /**
